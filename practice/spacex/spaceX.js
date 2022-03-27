b5a71cc6d0d01ec737e9ca88749b8d0ab530b6f5
yangya19 <95553242+yangya19@users.noreply.github.com>
2022-03-26 23:04:29 -0400

    Challenge Update

--- practice/spacex/spaceX.js
+++
@@ -1,6 +0,0 @@
-// const url = "https://api.spacexdata.com/v2/launchpads";
-// d3.json(url).then(receivedData => console.log(receivedData))
-
-d3.json("samples.json").then(function(data){
-    console.log(data);
-});
